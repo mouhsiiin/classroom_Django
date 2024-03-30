6a51bb0481fc4039a4f6dfcebf61b3511342c7b7
mouhsiiin <81185833+mouhsiiin@users.noreply.github.com>
2024-03-30 15:55:51 +0000

    updates the login urls

--- InTouchLearn/main/urls.py
+++ InTouchLearn/main/urls.py
@@ -5,9 +5,9 @@ from socialmedia import views as social_views
 
 urlpatterns = [
     path("", social_views.post_list_view , name="post-list"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
-    path("logout/", views.logout, name="logout"),
+    path("accounts/login/", views.login, name="login"),
+    path("accounts/register/", views.register, name="register"),
+    path("accounts/logout/", views.logout, name="logout"),
     path("confirm_email/<uidb64>/<token>", views.confirm_email, name="confirm_email"),
 
 ]
